e800589021cc8d866748e0f793468e33db20e4eb
photino <panzan89@gmail.com>
2015-05-29 21:35:02 +0800

    Updated Gauss–Legendre quadrature

--- src/quadrature.rs
+++ src/quadrature.rs
@@ -8,26 +8,25 @@
 /// use std::f64;
 /// use integration::quadrature;
 ///
-/// fn f(x: f64) -> f64 {
-///     x.sin()
-/// }
+/// let f = &|x: f64| x.sin();
 ///
-/// let y = quadrature::gauss_legendre(f, 0_f64, f64::consts::PI, 1e-10);
+/// let y = quadrature::gauss_legendre(f, 0.0, f64::consts::PI, 1e-10);
 ///
 /// assert!((y - 2.0).abs() < 1e-10);
 /// ```
-pub fn gauss_legendre(f: fn(f64) -> f64, a: f64, b: f64, eps: f64) -> f64 {
-    let x1 = -0.745355992499929898803057889577092078_f64;
+pub fn gauss_legendre(f: &Fn(f64) -> f64, a: f64, b: f64, eps: f64) -> f64 {
+    let k = 0.6_f64;
+    let x1 = k.sqrt();
     let x2 = 0_f64;
-    let x3 = 0.745355992499929898803057889577092078_f64;
-    let w1 = 0.555555555555555555555555555555555556_f64;
-    let w2 = 0.888888888888888888888888888888888889_f64;
-    let w3 = 0.555555555555555555555555555555555556_f64;
-    let m = 0.5 * (a + b);
-    let d = 0.5 * (b - a);
-    let y = d * (w1 * f(d * x1 + m) + w2 * f(d * x2 + m) + w3 * f(d * x3 + m));
-    if (y - d * (f(a) + f(b))).abs() > eps {
-        gauss_legendre(f, a, m, 0.5 * eps) + gauss_legendre(f, m, b, 0.5 * eps)
+    let x3 = -x1;
+    let w1 = 5.0 / 9.0;
+    let w2 = 8.0 / 9.0;
+    let w3 = w1;
+    let m = (a + b) / 2.0;
+    let d = (b - a) / 2.0;
+    let y = d * (w1 * f(x1.mul_add(d, m)) + w2 * f(x2.mul_add(d, m)) + w3 * f(x3.mul_add(d, m)));
+    if (y - d * (f(a) + 4.0 * f(m) + f(b)) / 3.0).abs() > eps {
+        gauss_legendre(f, a, m, eps / 2.0) + gauss_legendre(f, m, b, eps / 2.0)
     } else {
         y
     }
